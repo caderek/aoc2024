960181c17ee903a48619f5b060f9b0ec6e6d7bc2
Maciej Caderek <maciej.caderek@gmail.com>
2024-12-01 19:07:18 +0100

    Day 1 completed

---
+++ src/day01/index.py
@@ -0,0 +1,29 @@
+import re
+
+a = []
+b = []
+
+with open('src/day01/input.txt', 'r') as file:
+    items = [re.split(r'\s+', line.strip()) for line in file]
+
+    for item in items:
+        a.append(int(item[0]))
+        b.append(int(item[1]))
+
+a.sort()
+b.sort()
+
+distances = [abs(x - y) for x, y in zip(a, b)]
+
+occurences = {}
+
+for y in b:
+    if y in occurences:
+        occurences[y] += 1
+    else:
+        occurences[y] = 1
+
+scores = [x * occurences.get(x, 0) for x in a]
+
+print(sum(distances))
+print(sum(scores))
