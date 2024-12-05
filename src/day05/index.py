cac7f87b1964fb1c13d86d6674c3cf6da89cf3cd
Maciej Caderek <maciej.caderek@gmail.com>
2024-12-05 18:04:39 +0100

    Day 5 completed

---
+++ src/day05/index.py
@@ -0,0 +1,29 @@
+import re
+from functools import cmp_to_key
+
+with open('src/day05/input.txt', 'r') as file:
+    rules_raw, manuals_raw = file.read().strip().split('\n\n')
+    rules = set(rules_raw.split('\n'))
+    manuals = [
+        [int(x) for x in re.findall("\d+", line)]
+        for line in manuals_raw.split('\n')
+    ]
+
+
+def compare(a, b):
+    return -1 if f"{a}|{b}" in rules else 0
+
+key = cmp_to_key(compare)
+
+def sorted_pages(manual):
+    return sorted(manual, key=key)
+
+def get_mid(items):
+    return items[len(items) // 2]
+
+part1 = sum(get_mid(m) for m in manuals if sorted_pages(m) == m)
+part2 = sum(get_mid(m) for m in map(sorted_pages, manuals)) - part1
+
+print(part1)
+print(part2)
+
