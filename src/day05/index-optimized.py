6eaf388553df8b5105d6d1cc5aff3cc561498ccc
Maciej Caderek <maciej.caderek@gmail.com>
2024-12-06 00:33:23 +0100

    Day 5 refactor

--- src/day05/index-optimized.py
+++ src/day05/index-optimized.py
@@ -1,4 +1,5 @@
 from functools import cmp_to_key
+from itertools import pairwise
 
 with open('input.txt') as file:
     rules_raw, manuals_raw = file.read().split('\n\n')
@@ -10,14 +11,23 @@ def compare(a, b):
 
 key = cmp_to_key(compare)
 
+def is_sorted(manual):
+    return all(compare(a, b) == -1 for a, b in pairwise(manual))
+
 def sorted_pages(manual):
     return sorted(manual, key=key)
 
 def get_mid(items):
     return int(items[len(items) // 2])
 
-part1 = sum(get_mid(m) for m in manuals if sorted_pages(m) == m)
-part2 = sum(get_mid(m) for m in map(sorted_pages, manuals)) - part1
+part1 = 0
+part2 = 0
+
+for m in manuals:
+    if is_sorted(m):
+        part1 += get_mid(m)
+    else:
+        part2 += get_mid(sorted_pages(m))
 
 print(part1)
 print(part2)
