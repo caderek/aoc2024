cef30acbc728ef861b1f08bc99ea6961c874aa74
Maciej Caderek <maciej.caderek@gmail.com>
2024-12-07 22:26:44 +0100

    Day 7 completed

---
+++ src/day07/index.py
@@ -0,0 +1,35 @@
+import re
+from itertools import product
+
+with open('input.example.txt') as file:
+    input = [
+        [int(x) for x in re.findall("\d+", line)]
+        for line in  file.read().strip().splitlines()
+    ]
+
+calc = {
+    '+': lambda a, b: a + b,
+    '*': lambda a, b: a * b,
+    '.': lambda a, b: int(f"{a}{b}"),
+}
+
+def solve(symbols):
+    sum = 0
+
+    for left, *nums in input:
+        first, *rest = nums
+        for ops in product(symbols, repeat=len(rest)):
+            right = first
+            for op, num in zip(ops, rest):
+                right = calc[op](right, num)
+            if left == right:
+                sum += left
+                break
+    return sum
+
+part1 = solve('+*')
+part2 = solve('+*.')
+
+print(part1)
+print(part2)
+
