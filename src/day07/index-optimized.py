77679e5e5306651fa43c73e7c10c5dad1b6ee148
Maciej Caderek <maciej.caderek@gmail.com>
2024-12-08 03:00:59 +0100

    Day 7 optimized

--- src/day07/index-optimized.py
+++ src/day07/index-optimized.py
@@ -8,30 +8,29 @@ with open('input.txt') as file:
         for line in  file.read().strip().splitlines()
     ]
 
-calc = {
-    '+': lambda a, b: a + b,
-    '*': lambda a, b: a * b,
-    '.': lambda a, b: a * 10 ** (math.ceil(math.log10(b+1))) + b,
-}
+calc = (
+    lambda a, b: a + b,
+    lambda a, b: a * b,
+    lambda a, b: a * 10 ** (math.ceil(math.log10(b+1))) + b,
+)
 
 def solve(symbols):
     sum = 0
 
     for left, *nums in input:
-        first, *rest = nums
-        for ops in product(symbols, repeat=len(rest)):
-            right = first
-            for op, num in zip(ops, rest):
+        for ops in product(symbols, repeat=len(nums) - 1):
+            right = nums[0]
+            for i in range(1, len(nums)):
                 if right > left:
                     continue
-                right = calc[op](right, num)
+                right = calc[ops[i - 1]](right, nums[i])
             if left == right:
                 sum += left
                 break
     return sum
 
-part1 = solve('+*')
-part2 = solve('+*.')
+part1 = solve((0, 1))
+part2 = solve((0, 1, 2))
 
 print(part1)
 print(part2)
