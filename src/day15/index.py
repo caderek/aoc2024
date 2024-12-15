fa092d1ac5af42acdebcb439bbbd666c6d7857fb
Maciej Caderek <maciej.caderek@gmail.com>
2024-12-15 20:38:05 +0100

    Day 15 part 2 completed

--- src/day15/index.py
+++ src/day15/index.py
@@ -3,53 +3,8 @@ import re
 from itertools import chain
 from copy import deepcopy
 
-sys.setrecursionlimit(1000000)
-
 with open('input.txt') as file:
     input = file.read().strip()
-
-input = """##########
-#..O..O.O#
-#......O.#
-#.OO..O.O#
-#..O@..O.#
-#O#..O...#
-#O..O..O.#
-#.OO.O.OO#
-#....O...#
-##########
-
-<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
-vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
-><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
-<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
-^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
-^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
->^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
-<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
-^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
-v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
-
-# input = """########
-# #..O.O.#
-# ##@.O..#
-# #...O..#
-# #.#.O..#
-# #...O..#
-# #......#
-# ########
-#
-# <^^>>>vv<v>>v<<"""
-
-input = """#######
-#...#.#
-#.....#
-#..OO@#
-#..O..#
-#.....#
-#######
-
-<vv<<^^<<^^"""
 
 dirs = {
     '<': (0, -1),
@@ -58,6 +13,12 @@ dirs = {
     'v': (1, 0),
 }
 
+def find_start(w):
+    for y in range(len(w)):
+        for x in range(len(w[0])):
+            if expanded[y][x] == '@':
+                return (y, x)
+
 def sum_coords(w, sym):
     res = 0
     for y in range(len(w)):
@@ -65,11 +26,6 @@ def sum_coords(w, sym):
             if w[y][x] == sym:
                 res += 100 * y + x
     return res
-
-def pprint(grid):
-    for line in grid:
-        print(''.join(line))
-    print('-' * len(grid))
 
 def double(cell):
     if cell == '@':
@@ -95,16 +51,13 @@ def solve1(w, moves, start):
             ry, rx = yy, xx
             moved = True
 
-        # print('Move:', move)
-        # pprint(w)
-
     return moved
 
-def solve2(w, moves, start, main = False):
+def solve2(w, moves, start):
     ry, rx = start
     moved = False
 
-    for move in moves:
+    for i, move in enumerate(moves):
         dy, dx = dirs[move]
         yy, xx = ry + dy, rx + dx
         if w[yy][xx] == '#':
@@ -116,7 +69,9 @@ def solve2(w, moves, start, main = False):
             w[yy][xx], w[ry][rx] = w[ry][rx], w[yy][xx]
             ry, rx = yy, xx
             moved = True
-        else:
+        elif move in ['^', 'v']:
+            # Check if both sides of the box can move
+            # before applying change to the main grid
             side_dir = 1 if w[yy][xx] == '[' else -1
             w_copy = deepcopy(w)
             side1_moved = solve2(w_copy, [move], (yy, xx))
@@ -129,38 +84,17 @@ def solve2(w, moves, start, main = False):
                 ry, rx = yy, xx
                 moved = True
 
-        if main:
-            print('Move:', move)
-            pprint(w)
-
     return moved
 
-a, b = input.split('\n\n')
+raw_warehouse, raw_moves = input.split('\n\n')
 
-warehouse = [[*line] for line in a.splitlines()]
+warehouse = [[*line] for line in raw_warehouse.splitlines()]
 expanded = expand_warehouse(deepcopy(warehouse))
 
-moves = [*"".join(b.splitlines())]
+moves = [*"".join(raw_moves.splitlines())]
 
-start1 = None
-
-for y in range(len(warehouse)):
-    for x in range(len(warehouse[0])):
-        if warehouse[y][x] == '@':
-            start1 = (y, x)
-
-start2 = None
-
-for y in range(len(expanded)):
-    for x in range(len(expanded[0])):
-        if expanded[y][x] == '@':
-            start2 = (y, x)
-
-pprint(warehouse)
-pprint(expanded)
-
-solve1(warehouse, moves, start1)
-solve2(expanded, moves, start2, True)
+solve1(warehouse, moves, find_start(warehouse))
+solve2(expanded, moves, find_start(expanded))
 
 print(sum_coords(warehouse, 'O'))
 print(sum_coords(expanded, '['))
